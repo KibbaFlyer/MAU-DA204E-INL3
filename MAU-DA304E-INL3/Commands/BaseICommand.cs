32d1b7fd3827a227150b0ba8ed8d82c317866ca9
KibbaFlyer <84014093+KibbaFlyer@users.noreply.github.com>
2024-03-05 20:33:27 +0100

    Initial commit

---
+++ MAU-DA304E-INL3/Commands/BaseICommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MAU_DA304E_INL3.Commands
+{
+    public class BaseICommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public BaseICommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
---
+++ MAU-DA304E-INL3/UnitTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAU_DA304E_INL3
+{
+    enum UnitTypes
+    {
+        Metric,
+        Imperial
+    }
+}
